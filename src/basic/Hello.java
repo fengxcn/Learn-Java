9f7db48eea1201e55ef85ff8ea8ebf15379b4c7d
Xuan Feng <fxuanf@cn.ibm.com>
2021-09-26 22:35:19 +0800

    添加注释

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -1,10 +1,29 @@
 package basic;
 
-public class Hello {
+/**
+ * 
+ * @author fengxuan
+ * 
+ * 可以用来自动创建文档的注释
+ */
+public class Hello { // 类名是Hello
+	
+	/*
+	 * 类名要求：
+	 * 类名必须以英文字母开头，后接字母，数字和下划线的组合
+	 * 习惯以大写字母开头
+	 * 好的类命名：Hello，NoteBook
+	 * 不好的类命名：hello，Hello1234，Note_Book
+     */
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 单行注释 向屏幕输出文本:
 		System.out.println("Hello World.");
+		
+		/* 多行注释开始
+        注释内容
+        注释结束 */
 	}
 
-}
+} // class定义结束
